972182deb19981f5f032dc943c05b06201bcde7c
Ayush Sharma <ayush.sharma3@gartner.com>
2024-07-27 16:01:16 +0530

    Changes to readme

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,24 +1,18 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
-
 
 require('lspconfig').lua_ls.setup({})
 require('lspconfig').pylsp.setup({})
 
-
 -- to learn how to use mason.nvim
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+    ensure_installed = {}
 })
--- nvim/lua/ayush/packer.lua
+++ nvim/lua/ayush/packer.lua
@@ -26,16 +26,17 @@ return require('packer').startup(function(use)
 	use('tpope/vim-fugitive')
 	use {'VonHeikemen/lsp-zero.nvim',
   		branch = 'v3.x',
- 	 requires = {
+ 	    requires = {
     	--- Uncomment the two plugins below if you want to manage the language servers from neovim
-	{'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
+	        {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
-  }
+            {'neovim/nvim-lspconfig'},
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+    }
 }
     use 'm4xshen/autoclose.nvim'
+    use 'numToStr/Comment.nvim'
 end)
