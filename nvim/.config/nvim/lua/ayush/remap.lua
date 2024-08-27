e9ea31446b8ac60be543de07d46144fe2137fae1
Ayush Sharma <ayush.sharma3@gartner.com>
2024-08-28 00:07:24 +0530

    Adding tmux config

--- nvim/.config/nvim/lua/ayush/remap.lua
+++ nvim/.config/nvim/lua/ayush/remap.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+
+-- move code in visual block
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
