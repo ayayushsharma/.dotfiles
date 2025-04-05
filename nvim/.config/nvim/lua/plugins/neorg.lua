29c9dca4a995b6392da6d8fce1796552501ea7e9
Ayush Sharma <ayush.sharma3@gartner.com>
2025-04-06 02:53:40 +0530

    [neovim] Switching to Lazy Package Manager

---
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,25 @@
+return {
+	"nvim-neorg/neorg",
+	lazy = false,
+	version = "*",
+	config = function()
+		HOME_DIR = os.getenv("HOME")
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {},
+				["core.concealer"] = {
+					config = {
+						icon_preset = "varied",
+					},
+				},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							todo = HOME_DIR .. "/todo",
+						},
+					},
+				},
+			},
+		})
+	end,
+}
