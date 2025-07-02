e0287073c760885c3e333254bde3a4326c1c96e0
ayayushsharma <64801786+ayayushsharma@users.noreply.github.com>
2025-07-02 19:57:30 +0530

    [zsh] Fixing fzf for more directories
    - nvim now support hidden files in telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -11,7 +11,19 @@ return {
         end)
 
         require("telescope").setup({
-            defaults = { file_ignore_patterns = { "node_modules", "__pycache__", ".venv" } },
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    "__pycache__",
+                    ".venv",
+                    ".git",
+                }
+            },
+            pickers = {
+                find_files = {
+                    hidden = true
+                }
+            }
         })
     end,
 }
