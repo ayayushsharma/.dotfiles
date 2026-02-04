cc66a4efc7eeff3e9b0a51466ca3581c68ec4fe4
ayayushsharma <64801786+ayayushsharma@users.noreply.github.com>
2026-02-04 15:02:47 +0530

    [nvim] Adding Java LSP

--- nvim/.config/nvim/lua/plugins/javalsp.lua
+++ nvim/.config/nvim/lua/plugins/javalsp.lua
@@ -1,13 +1,18 @@
 return {
     "nvim-java/nvim-java",
     config = function()
-        -- local language = string.upper("java")
-        -- local env_var = "IS_INSTALLED_" .. language
-        -- local env_value = os.getenv(env_var)
-        -- if env_value == nil then
-        --     return
-        -- end
-        -- require("java").setup()
-        -- vim.lsp.enable("jdtls")
+        local language = string.upper("java")
+        local env_var = "IS_INSTALLED_" .. language
+        local env_value = os.getenv(env_var)
+        if env_value == nil then
+            return
+        end
+        require('java').setup({
+            jdk = { auto_install = false },
+            java_debug_adapter = { enable = false },
+            java_test = { enable = false },
+            spring_boot_tools = { enable = false },
+        })
+        vim.lsp.enable('jdtls')
     end,
 }
